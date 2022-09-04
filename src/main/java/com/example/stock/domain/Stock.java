36eb80fe78f135950a77171f39e89eb9809a302b
kimjinwook1 <rlawlsdnr628@naver.com>
2022-09-04 16:09:06 +0900

    Optimistic Lock 활용

--- src/main/java/com/example/stock/domain/Stock.java
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,6 +22,8 @@ public class Stock {
 
 	private Long productId;
 	private Long quantity;
+	@Version
+	private Long version;
 
 
 	public void decrease(Long quantity) {
--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.stock.domain.Stock;
+import com.example.stock.facade.OptimisticLockStockFacade;
 import com.example.stock.repository.StockRepository;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -24,6 +25,9 @@ class StockServiceTest {
 
 	@Autowired
 	private PessimisticLockStockService pessimisticLockStockService;
+
+	@Autowired
+	OptimisticLockStockFacade optimisticLockStockFacade;
 
 	@BeforeEach
 	void before() {
@@ -87,6 +91,32 @@ class StockServiceTest {
 			executorService.submit(() -> {
 				try {
 					pessimisticLockStockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+
+		Stock findStock = stockRepository.findById(1L).orElseThrow();
+
+		//100 - (1*100) = 0
+		assertThat(findStock.getQuantity()).isEqualTo(0L);
+	}
+
+	@Test
+	void 동시에_100개의_요청_Optimistic_Lock() throws InterruptedException {
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					optimisticLockStockFacade.decrease(1L, 1L);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
 				} finally {
 					latch.countDown();
 				}
