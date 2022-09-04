3df69750efb98093f5b9867a346b9c4234115388
kimjinwook1 <rlawlsdnr628@naver.com>
2022-09-04 15:20:08 +0900

    동시성 문제 발생

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -45,5 +48,30 @@ class StockServiceTest {
 
 		//then
 		assertThat(findStock.getQuantity()).isEqualTo(99L);
+	}
+
+	@Test
+	void 동시에_100개의_요청() throws InterruptedException {
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+
+		Stock findStock = stockRepository.findById(1L).orElseThrow();
+
+		//100 - (1*100) = 0
+		assertThat(findStock.getQuantity()).isEqualTo(0L);
+
 	}
 }
